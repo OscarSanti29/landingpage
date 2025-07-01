2c2d747617b2686205f1a76e7ceff7aeb1c73af2
OscarSanti29 <osantiesteban69@hotmail.com>
2025-06-30 20:53:31 -0500

    starting landing page

--- src/app/components/Landing.jsx
+++ src/app/components/Landing.jsx
@@ -1,3 +1,28 @@
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+
 export default function Landingpage() {
-  return <div>Canvas Bakery</div>;
+  return (
+    <section className="p-8 text-center">
+      <Image
+        src="/Logo.jpg"
+        alt="Bakery Logo"
+        className="rounded-lg m-auto"
+        width={600}
+        height={400}
+      />
+
+      <div className="mt-8">
+        <h1 className="text-3xl font-bold mb-2">About Me</h1>
+        <p className="text-lg">I love to bake!!!!!</p>
+      </div>
+
+      <div className="mt-8">
+        <h1 className="text-3xl font-bold mb-2">Contact Me!!</h1>
+
+        <FontAwesomeIcon icon={faInstagram} className="w-8 h-8" />
+      </div>
+    </section>
+  );
 }
